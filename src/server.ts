df57c4a25c24574548b9c8565da05b5e7eccc5b1
Kasra Karami <Kasra_K2K@yahoo.com>
2023-06-11 12:24:51 +0330

    add graphql

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,44 @@
+/* ------------------------------ Dependencies ------------------------------ */
 import Koa from 'koa'
-const app = new Koa()
+import { ApolloServer } from '@apollo/server'
+import { startStandaloneServer } from '@apollo/server/standalone'
+/* --------------------------------- Modules -------------------------------- */
 import router from './http/router'
+/* -------------------------------------------------------------------------- */
 
-const port = process.env.PORT || 3000
+const app = new Koa()
+const PORT = Number(process.env.PORT) || 3000
+const GRAPHQL_PORT = Number(process.env.GRAPHQL_PORT) || 4000
 
+// The GraphQL schema
+const typeDefs = `#graphql
+  type Query {
+    hello: String
+  }
+`
+
+// A map of functions which return data for the schema.
+const resolvers = {
+  Query: {
+    hello: () => 'world',
+  },
+}
+
+/* -------------------------------------------------------------------------- */
+/*                                   GraphQL                                  */
+/* -------------------------------------------------------------------------- */
+const server = new ApolloServer({ typeDefs, resolvers })
+startStandaloneServer(server, { listen: { port: GRAPHQL_PORT } }).then(({ url }) =>
+  console.log(`🟢 GraphQL Server ready at:\t ${url}`)
+)
+
+/* -------------------------------------------------------------------------- */
+/*                                  REST API                                  */
+/* -------------------------------------------------------------------------- */
 app
   .use(router.routes())
   .use(router.allowedMethods())
-  .listen(port, () => console.log(`Server running on http://localhost:${port}`))
+  .listen(PORT, () => console.log(`🟢 Rest server ready at:\t http://localhost:${PORT}/`))
 
 app.on('error', (err, ctx) => {
   console.error('server error', { err, ctx })
