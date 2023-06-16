46d8fe07401b5927c287baa46748fc17cfe230f7
Kasra Karami <Kasra_K2K@yahoo.com>
2023-06-16 09:32:49 +0330

    raise error inside the modules

--- src/restful/router/index.ts
+++ src/restful/router/index.ts
@@ -1,8 +1,18 @@
 import Router from '@koa/router'
+import NotFoundError from '../../common/helpers/errors/NotFoundError'
 const router = new Router()
 
 router.get('/', (ctx): void => {
   ctx.body = { message: 'Hello World' }
+  myError()
 })
+
+function myError() {
+  B()
+}
+
+function B() {
+  throw new NotFoundError('Error happened when getting root path')
+}
 
 export default router
