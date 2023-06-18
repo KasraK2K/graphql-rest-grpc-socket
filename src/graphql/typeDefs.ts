8981dfd50b4cad47593f65bbfa2887a9af8ad2f2
Kasra Karami <Kasra_K2K@yahoo.com>
2023-06-18 12:32:03 +0330

    add schema loader for microservices schema

--- src/graphql/typeDefs.ts
+++ src/graphql/typeDefs.ts
@@ -5,6 +5,18 @@ import { typeDefs as scalarTypeDefs } from 'graphql-scalars'
 const globalSchema = loadFilesSync(join(__dirname, './global.graphql'))
 const subSchemas = loadFilesSync(join(process.cwd(), 'src/modules/**/*.graphql'))
 
+// ─────────────────────────────────────────────────────────────────────────────────────
+//   :::::: NOTE: Microservice External Schema : :  :   :    :     :        :          :
+// ─────────────────────────────────────────────────────────────────────────────────────
+// import { loadSchemaSync } from '@graphql-tools/load'
+// import { UrlLoader } from '@graphql-tools/url-loader'
+
+// const microserviceExternalSchema = loadSchemaSync('https://api.spacex.land/graphql/', {
+//   loaders: [new UrlLoader()],
+// })
+// subSchemas.push(microserviceExternalSchema)
+// ─────────────────────────────────────────────────────────────────────────────────────
+
 const typeDefs = [...scalarTypeDefs, globalSchema, subSchemas]
 
 export default typeDefs
