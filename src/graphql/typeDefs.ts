e159c60761714545e7233dc0551850567a738dd5
Kasra Karami <Kasra_K2K@yahoo.com>
2023-06-18 12:19:40 +0330

    add scalar to our types

--- src/graphql/typeDefs.ts
+++ src/graphql/typeDefs.ts
@@ -1,9 +1,10 @@
 import { join } from 'node:path'
 import { loadFilesSync } from '@graphql-tools/load-files'
+import { typeDefs as scalarTypeDefs } from 'graphql-scalars'
 
 const globalSchema = loadFilesSync(join(__dirname, './global.graphql'))
 const subSchemas = loadFilesSync(join(process.cwd(), 'src/modules/**/*.graphql'))
 
-const typeDefs = [globalSchema, subSchemas]
+const typeDefs = [...scalarTypeDefs, globalSchema, subSchemas]
 
 export default typeDefs
