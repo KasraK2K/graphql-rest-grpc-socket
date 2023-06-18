ea20547ba9034225f911a6da2b6c9c83c9092d74
Kasra Karami <Kasra_K2K@yahoo.com>
2023-06-18 13:23:17 +0330

    remove unnecessary things and add type to loadFileSync

--- src/graphql/typeDefs.ts
+++ src/graphql/typeDefs.ts
@@ -3,21 +3,9 @@ import { loadFilesSync } from '@graphql-tools/load-files'
 import { typeDefs as scalarTypeDefs } from 'graphql-scalars'
 import { GraphQLSchema } from 'graphql'
 
-const globalSchema = loadFilesSync(join(__dirname, './global.graphql'))
-const subSchemas = loadFilesSync(join(process.cwd(), 'src/modules/**/*.graphql'))
+const globalSchema = loadFilesSync<GraphQLSchema>(join(__dirname, './global.graphql'))
+const subSchemas = loadFilesSync<GraphQLSchema>(join(process.cwd(), 'src/modules/**/*.graphql'))
 
-// ─────────────────────────────────────────────────────────────────────────────────────
-//   :::::: NOTE: Microservice External Schema : :  :   :    :     :        :          :
-// ─────────────────────────────────────────────────────────────────────────────────────
-// import { loadSchemaSync } from '@graphql-tools/load'
-// import { UrlLoader } from '@graphql-tools/url-loader'
-
-// const microserviceExternalSchema = loadSchemaSync('https://ch1.graphql.guide/graphql', {
-//   loaders: [new UrlLoader()],
-// })
-// subSchemas.push(microserviceExternalSchema)
-// ─────────────────────────────────────────────────────────────────────────────────────
-
-const typeDefs = [...scalarTypeDefs, globalSchema, ...subSchemas]
+const typeDefs = [...globalSchema, ...subSchemas, ...scalarTypeDefs]
 
 export default typeDefs
