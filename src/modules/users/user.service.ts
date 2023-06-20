105d27bab5a788cedbd388bd35cfce1f48ef76b3
Kasra Karami <Kasra_K2K@yahoo.com>
2023-06-20 18:19:45 +0330

    remove loop for now to find a way to cach requests

--- src/modules/users/user.service.ts
+++ src/modules/users/user.service.ts
@@ -4,13 +4,13 @@
 import { GraphQLResolveInfo } from 'graphql'
 import { Context } from '../../graphql/context'
 import { IUser } from './constants/interfaces'
-// import graphErrorHandler from '../../common/helpers/errors/error.handler'
+import graphErrorHandler from '../../common/helpers/errors/error.handler'
 
 class UserService {
   findAll({ dataSource }: Context, info: GraphQLResolveInfo): IUser[] {
     // graphErrorHandler(info, 410)
-    for (const item in Array(10000000).fill(0)) {
-    }
+    // for (const item in Array(10000000).fill(0)) {
+    // }
     return dataSource.user.repo.findAll()
   }
 }
