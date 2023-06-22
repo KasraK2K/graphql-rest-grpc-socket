281fff33a2f4fc5e5c08b28750432101a87d6bc9
Kasra Karami <Kasra_K2K@yahoo.com>
2023-06-23 00:48:10 +0330

    create rest child app

---
+++ src/applications/rest.app.ts
@@ -0,0 +1,62 @@
+//============================================================
+//
+//  #####    #####   ####  ######          ###    #####   ##
+//  ##  ##   ##     ##       ##           ## ##   ##  ##  ##
+//  #####    #####   ###     ##          ##   ##  #####   ##
+//  ##  ##   ##        ##    ##          #######  ##      ##
+//  ##   ##  #####  ####     ##          ##   ##  ##      ##
+//
+//============================================================
+// Rest server function using Sofa
+//============================================================
+
+/* ------------------------------ Node Modules ------------------------------ */
+import { createServer } from 'node:http'
+/* ------------------------------ Dependencies ------------------------------ */
+import express from 'express'
+import { useSofa } from 'sofa-api'
+/* ----------------------------- Custom Modules ----------------------------- */
+import schema from '../graphql/schema'
+import { context } from '../graphql/context'
+import colour from '../common/utils/logColour.util'
+/* -------------------------------- Constants ------------------------------- */
+const REST_PORT = process.env.PORT || '3500'
+/* -------------------------------------------------------------------------- */
+
+/* -------------------------------------------------------------------------- */
+/*                                 Rest Server                                */
+/* -------------------------------------------------------------------------- */
+function main() {
+  const app = express()
+  const sofa = useSofa({
+    schema,
+    context,
+    basePath: '/',
+    swaggerUI: { endpoint: '/swagger' },
+    openAPI: {
+      info: { title: 'Graphql Boilerplate API', version: '1.0.0' },
+      endpoint: '/openapi.json',
+    },
+    routes: {
+      'Query.users': { method: 'GET' },
+    },
+  })
+
+  app.use('/', sofa)
+  const restAppServer = createServer(app)
+
+  restAppServer
+    .listen(REST_PORT)
+    .on('listening', () =>
+      console.info(
+        `${colour.love('HTTP')}\t server ready at: ${colour.love.underline(
+          `http://localhost:${REST_PORT}`
+        )}`
+      )
+    )
+    .on('error', (err) => {
+      console.error('on error', err)
+    })
+}
+
+export default main
