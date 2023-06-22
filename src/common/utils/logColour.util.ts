a875fdec55045ab1333cf00883d96ca098d2a696
Kasra Karami <Kasra_K2K@yahoo.com>
2023-06-23 00:48:33 +0330

    add colour util

---
+++ src/common/utils/logColour.util.ts
@@ -0,0 +1,10 @@
+/* ------------------------------ Dependencies ------------------------------ */
+import chalk from 'chalk'
+/* -------------------------------------------------------------------------- */
+
+const love = chalk.hex('#f6009b')
+const green = chalk.greenBright
+const yellow = chalk.yellowBright
+const red = chalk.redBright
+
+export default { love, green, yellow, red }
