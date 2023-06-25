ab3debe25796f8ef547253c379252d540bcc9f1c
Kasra Karami <Kasra.Karami.Work@gmail.com>
2023-06-25 12:20:41 +0330

    git fix

---
+++ src/common/interfaces/repository.interface.ts
@@ -0,0 +1,25 @@
+export interface IExecuteQueryOptions {
+  query: string
+  parameters?: any[]
+  omits?: string[]
+}
+
+export interface IExecuteOptions {
+  omits?: string[]
+}
+
+export interface IQueryGenerator {
+  query: string
+  parameters: any[]
+}
+
+export interface IPaginate {
+  rowCount: number
+  rows: Record<string, any>[]
+  total_count?: number
+  total_page?: number
+  page?: number
+  limit?: number
+  nextPage?: number
+  prevPage?: number
+}
