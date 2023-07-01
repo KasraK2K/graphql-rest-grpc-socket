492f8eaec6e83ca4d0cb5740e901797a5632b54a
Kasra Karami <Kasra_K2K@yahoo.com>
2023-07-01 13:44:00 +0330

    prettier fix

--- knexfile.ts
+++ knexfile.ts
@@ -1,47 +1,46 @@
-import type { Knex } from "knex";
+import type { Knex } from 'knex'
 
 // Update with your config settings.
 
 const config: { [key: string]: Knex.Config } = {
   development: {
-    client: "sqlite3",
+    client: 'sqlite3',
     connection: {
-      filename: "./dev.sqlite3"
-    }
+      filename: './dev.sqlite3',
+    },
   },
 
   staging: {
-    client: "postgresql",
+    client: 'postgresql',
     connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
+      database: 'my_db',
+      user: 'username',
+      password: 'password',
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: "knex_migrations"
-    }
+      tableName: 'knex_migrations',
+    },
   },
 
   production: {
-    client: "postgresql",
+    client: 'postgresql',
     connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
+      database: 'my_db',
+      user: 'username',
+      password: 'password',
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: "knex_migrations"
-    }
-  }
+      tableName: 'knex_migrations',
+    },
+  },
+}
 
-};
-
-module.exports = config;
+module.exports = config
