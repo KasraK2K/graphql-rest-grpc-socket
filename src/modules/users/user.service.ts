30900095aa2387b66e19035ab958106e0e2f2e8d
Kasra Karami <Kasra_K2K@yahoo.com>
2023-07-02 03:19:58 +0330

    hash user password

--- src/modules/users/user.service.ts
+++ src/modules/users/user.service.ts
@@ -1,10 +1,12 @@
 /* ----------------------------- Custom Modules ----------------------------- */
 import userRepository from './user.repository'
 import { IUser } from './constants/interfaces'
+import bcryptHelper from '../../common/helpers/bcrypt.helper'
 /* -------------------------------------------------------------------------- */
 
 class UserService {
     async addUser(args: { email: string; password: string }): Promise<IUser> {
+        args.password = bcryptHelper.hashGen(args.password)
         return await userRepository.addUser(args)
     }
 
