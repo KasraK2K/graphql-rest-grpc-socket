71bffd8ff89615bb91feb91ebf07862cc01b27aa
Kasra Karami <Kasra_K2K@yahoo.com>
2023-07-02 01:49:53 +0330

    fix error file path

--- src/common/helpers/errors/errorFilePath.ts
+++ src/common/helpers/errors/errorFilePath.ts
@@ -7,18 +7,23 @@ const errorFilePath = (error: unknown | Error | GraphQLError) => {
         const stack = error.stack.split('\n').map((line) => line.trim())
         let line: string
 
-        error instanceof GraphQLError || error instanceof Error
-            ? (line = stack[3])
-            : (line = stack[1])
+        line =
+            error instanceof GraphQLError || error instanceof Error
+                ? (line = stack[3].trim())
+                : (line = stack[1].trim())
 
-        const file_path = line.substring(
-            line.indexOf('(') + process.cwd().length + 2,
-            line.lastIndexOf(')')
-        )
-        const file_name = file_path.substring(
-            file_path.lastIndexOf('/') + 1,
-            file_path.indexOf(':')
-        )
+        const hasParentheses = line.endsWith(')')
+        let file_path: string
+        let file_name: string
+
+        if (hasParentheses) {
+            file_path = line.slice(line.indexOf('/src'), line.length - 1)
+            file_name = line.slice(line.lastIndexOf('/') + 1, line.indexOf(':'))
+        } else {
+            file_path = line.slice(line.indexOf('/src'))
+            file_name = line.slice(line.lastIndexOf('/') + 1, line.indexOf(':'))
+        }
+
         return { file_name, file_path }
     } else return { file_name: 'unknown', file_path: 'unknown' }
 }
