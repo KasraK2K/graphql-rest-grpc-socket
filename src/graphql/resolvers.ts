88f1255a3b41664d26e29b01546a4528ecf32a19
Kasra Karami <Kasra_K2K@yahoo.com>
2023-07-06 23:08:42 +0330

    change load resolvers because of error on build time

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,9 +1,7 @@
-/* ------------------------------ Node Modules ------------------------------ */
-import { join } from 'node:path'
 /* ------------------------------ Dependencies ------------------------------ */
 import { loadFilesSync } from '@graphql-tools/load-files'
 /* -------------------------------------------------------------------------- */
 
-const subResolvers = loadFilesSync(join(process.cwd(), 'src/modules/**/*.resolver.{js,ts}'))
+const subResolvers = loadFilesSync('../modules/**/*.resolver.{js,ts}')
 
 export default subResolvers
