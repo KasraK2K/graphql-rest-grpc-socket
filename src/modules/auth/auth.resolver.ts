f306a30dfac04e97b40f088e1787d9ff61202e0c
Kasra Karami <Kasra_K2K@yahoo.com>
2023-07-10 18:07:06 +0330

    add example subscription

--- src/modules/auth/auth.resolver.ts
+++ src/modules/auth/auth.resolver.ts
@@ -57,6 +57,18 @@ const resolvers = {
             const { token, user } = await authService.registerUser(args)
             return { token, user }
         }
+    },
+
+    Subscription: {
+        countdown: {
+            // This will return the value on every 1 sec until it reaches 0
+            subscribe: async function* (_, { from }) {
+                for (let i = from; i >= 0; i--) {
+                    await new Promise((resolve) => setTimeout(resolve, 1000))
+                    yield { countdown: i }
+                }
+            }
+        }
     }
 }
 
