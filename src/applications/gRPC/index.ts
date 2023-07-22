35355a8ef79f3bf4d583b95f7a35bdf295b93ddd
Kasra Karami <Kasra_K2K@yahoo.com>
2023-07-22 09:24:56 +0330

    remove unnecessary gRPC functions

--- src/applications/gRPC/index.ts
+++ src/applications/gRPC/index.ts
@@ -3,15 +3,11 @@ import { gRPCServerNames } from './constants/enums'
 /* -------------------------------------------------------------------------- */
 
 /* ------------------------------ Default gRPC ------------------------------ */
-import { startGreetingClient, startGreetingServer } from './servers/default/default.grpc'
+import { startGreetingServer } from './servers/default/default.grpc'
 /* -------------------------------------------------------------------------- */
 
 export const startGrpcServers = (serverNames: number[]) => {
     for (const name of serverNames) serverSwitch(name)
-}
-
-export const startGrpcClient = (clientNames: number[]) => {
-    for (const name of clientNames) clientSwitch(name)
 }
 
 /* SECTION --------------------------- Register Servers --------------------- */
@@ -19,16 +15,6 @@ const serverSwitch = (serverName: number) => {
     switch (serverName) {
         case gRPCServerNames.DEFAULT:
             startGreetingServer()
-            break
-    }
-}
-/* -------------------------------------------------------------------------- */
-
-/* SECTION -------------------- Register Clients ---------------------------- */
-const clientSwitch = (clientName: number) => {
-    switch (clientName) {
-        case gRPCServerNames.DEFAULT:
-            startGreetingClient()
             break
     }
 }
--- src/applications/gRPC/servers/default/default.grpc.ts
+++ src/applications/gRPC/servers/default/default.grpc.ts
@@ -5,24 +5,6 @@ import { grpc, grpcServer } from '../../constants/grpc.config'
 import './modules/auth/auth.service'
 /* -------------------------------------------------------------------------- */
 
-/* ------------------------------ Start Client ------------------------------ */
-export const startGreetingClient = () => {
-    grpcServer.bindAsync(
-        process.env.GRPC_ADDRESS,
-        grpc.ServerCredentials.createInsecure(),
-        (error, _port) => {
-            if (error) console.log(error)
-            console.log(
-                `${colour.love('gRPC')}\t client ready at: ${colour.green.underline(
-                    `${process.env.GRPC_ADDRESS}`
-                )}`
-            )
-            grpcServer.start()
-        }
-    )
-}
-/* -------------------------------------------------------------------------- */
-
 /* ------------------------------ Start Server ------------------------------ */
 export const startGreetingServer = () => {
     grpcServer.bindAsync(
