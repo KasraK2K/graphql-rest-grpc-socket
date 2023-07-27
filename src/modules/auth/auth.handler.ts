ab6a5d65c0a764523d6f2ad7001a8fd98b24abd1
Kasra Karami <Kasra_K2K@yahoo.com>
2023-07-27 20:37:09 +0330

    create auth handler

---
+++ src/modules/auth/auth.handler.ts
@@ -0,0 +1,72 @@
+/* ------------------------------ Dependencies ------------------------------ */
+import { GraphQLResolveInfo } from 'graphql'
+import _ from 'lodash'
+import config from 'config'
+/* ----------------------------- Custom Modules ----------------------------- */
+import authService from './auth.service'
+import { IContext } from '../../graphql/context'
+import { IUserAuthResponse, IAdminAuthResponse, ITokenPayload } from '../../common/interfaces'
+import tokenHelper from '../../common/helpers/token.helper'
+import errorHandler from '../../common/helpers/errors/error.handler'
+import { IApplicationConfig } from '../../../config/config.interface'
+import { Role } from '../../common/decorators'
+/* -------------------------------------------------------------------------- */
+
+const applicationConfig: IApplicationConfig = config.get('application')
+
+const getTokenAndPayload = (context: IContext): { token: string; data: ITokenPayload } => {
+    const authorization = context.request.headers.get(applicationConfig.bearerHeader)
+    if (authorization) {
+        const token = authorization.slice(applicationConfig.bearer.length + 1)
+        const { valid, data } = tokenHelper.verify(token)
+        if (!valid) throw errorHandler(403)
+        else return { token, data }
+    } else throw errorHandler(401)
+}
+
+class AuthHandler {
+    async loginAdmin(
+        _parent: IAdminAuthResponse,
+        args: { email: string; password: string },
+        _context: IContext,
+        _info: GraphQLResolveInfo
+    ): Promise<IAdminAuthResponse> {
+        const { token, admin } = await authService.loginAdmin(args)
+
+        return { token, admin }
+    }
+
+    @Role(['admin'])
+    async loginUser(
+        _parent: IUserAuthResponse,
+        args: { email: string; password: string },
+        _context: IContext,
+        _info: GraphQLResolveInfo
+    ): Promise<IUserAuthResponse> {
+        const { token, user } = await authService.loginUser(args)
+        return { token, user }
+    }
+
+    async registerAdmin(
+        _parent: IAdminAuthResponse,
+        args: { email: string; password: string },
+        context: IContext,
+        _info: GraphQLResolveInfo
+    ): Promise<IAdminAuthResponse> {
+        const { data } = getTokenAndPayload(context)
+        const { token, admin } = await authService.registerAdmin(data, args)
+        return { token, admin }
+    }
+
+    async registerUser(
+        _parent: IUserAuthResponse,
+        args: { email: string; password: string },
+        _context: IContext,
+        _info: GraphQLResolveInfo
+    ): Promise<IUserAuthResponse> {
+        const { token, user } = await authService.registerUser(args)
+        return { token, user }
+    }
+}
+
+export default new AuthHandler()
