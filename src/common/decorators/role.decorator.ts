e34a6dd9175d2c2766f2f4038d79a742dad6e99a
Kasra Karami <Kasra_K2K@yahoo.com>
2023-07-27 20:36:19 +0330

    fix role decorator

--- src/common/decorators/role.decorator.ts
+++ src/common/decorators/role.decorator.ts
@@ -14,17 +14,18 @@ const Role = (roles: string[]) => {
         const originalValue = descriptor.value
 
         descriptor.value = function (...args: any[]) {
-            const headers: string[] = Array.from(args[0].rawHeaders)
+            const rawHeaders = args[2].req.rawHeaders
+            const headers: string[] = Array.from(rawHeaders)
             let token: string | string[] = headers.filter((header) => header.startsWith(bearerKey))
 
             // Check Authorization Header
-            if (!token || !token.length || !headers.includes(bearerHeader)) errorHandler(401)
+            if (!token || !token.length || !headers.includes(bearerHeader)) throw errorHandler(401)
             // Check Token & Role
             else {
                 token = token[0].slice(bearerKey.length + 1)
                 const tokenRole = 'user' // FIXME : Extract from token
                 // Check role is valid
-                if (!roles.includes(tokenRole)) errorHandler(403)
+                if (!roles.includes(tokenRole)) throw errorHandler(403)
             }
 
             return originalValue.apply(this, args)
