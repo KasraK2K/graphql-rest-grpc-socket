bd53fd516f08bb774d65cce4fc8b105266ffc274
Kasra Karami <Kasra_K2K@yahoo.com>
2023-07-28 09:09:45 +0330

    fix auth resolver

--- src/modules/auth/auth.resolver.ts
+++ src/modules/auth/auth.resolver.ts
@@ -13,18 +13,18 @@ const resolvers = {
     Mutation: {
         registerAdmin: authHandler.registerAdmin,
         registerUser: authHandler.registerUser
+    },
 
-        // Subscription: {
-        //     countdown: {
-        //         // This will return the value on every 1 sec until it reaches 0
-        //         subscribe: async function* (_, { from }) {
-        //             for (let i = from; i >= 0; i--) {
-        //                 await new Promise((resolve) => setTimeout(resolve, 1000))
-        //                 yield { countdown: i }
-        //             }
-        //         }
-        //     }
-        // }
+    Subscription: {
+        countdown: {
+            // This will return the value on every 1 sec until it reaches 0
+            subscribe: async function* (_, { from }) {
+                for (let i = from; i >= 0; i--) {
+                    await new Promise((resolve) => setTimeout(resolve, 1000))
+                    yield { countdown: i }
+                }
+            }
+        }
     }
 }
 
