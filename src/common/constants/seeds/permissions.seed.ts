d722eee9c957852b525ba0b944758553c8f178ad
Kasra Karami <Kasra_K2K@yahoo.com>
2023-07-31 20:52:06 +0330

    add module number for permissions seeder

--- src/common/constants/seeds/permissions.seed.ts
+++ src/common/constants/seeds/permissions.seed.ts
@@ -1,14 +1,18 @@
+import { ModuleNumber } from '../../../common/enums/general.enum'
+
 const PERMISSIONS_SEED = [
     {
         id: 1,
         name: 'Default User Permissions',
         description: 'Default permissions for normal users',
+        module: ModuleNumber.USER,
         access: [1001]
     },
     {
         id: 2,
         name: 'Default Admin Permissions',
         description: 'Default permissions for normal users',
+        module: ModuleNumber.ADMIN,
         access: [1002]
     }
 ]
