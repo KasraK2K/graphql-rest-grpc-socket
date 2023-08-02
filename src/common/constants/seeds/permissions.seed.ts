f666fb1d51e0e6b2a074c9e10be229ed243c3f5e
Kasra Karami <Kasra_K2K@yahoo.com>
2023-08-02 19:32:56 +0330

    Change role and permissions default seeders

--- src/common/constants/seeds/permissions.seed.ts
+++ src/common/constants/seeds/permissions.seed.ts
@@ -3,17 +3,17 @@ import { ModuleNumber } from '../../../common/enums/general.enum'
 const PERMISSIONS_SEED = [
     {
         id: 1,
-        name: 'List of users',
-        description: 'Get list of all users',
-        module: ModuleNumber.USER,
-        access: 1001
-    },
-    {
-        id: 2,
         name: 'List of admins',
         description: 'Get list of all admins',
         module: ModuleNumber.ADMIN,
         access: 1002
+    },
+    {
+        id: 2,
+        name: 'List of users',
+        description: 'Get list of all users',
+        module: ModuleNumber.USER,
+        access: 1001
     }
 ]
 
--- src/common/constants/seeds/roles.seed.ts
+++ src/common/constants/seeds/roles.seed.ts
@@ -1,6 +1,6 @@
 const ROLES_SEED = [
-    { id: 1, name: 'User', description: 'Default roles for users', permissions: [1] },
-    { id: 2, name: 'Admin', description: 'Default roles for admins', permissions: [2] }
+    { id: 2, name: 'Admin', description: 'Default roles for admins', permissions: [1, 2] },
+    { id: 1, name: 'User', description: 'Default roles for users', permissions: [2] }
 ]
 
 export default ROLES_SEED
